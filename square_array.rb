4b40403a3649460721d2d6ad63b580b4e69b4f36
Morteza Rohaninejad <mortezaroha@gmail.com>
2019-09-15 13:42:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  
+#  i = 0
+ # while i < array.length
+ #   array[i] *= array[i]
+ #   i += 1
+ # end
+  #return array
+  array.each do |n|
+    n = n * n
+    puts n + "foo"
+  end
 end
